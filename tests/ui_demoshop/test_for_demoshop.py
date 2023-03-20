c16b2fee81b4d265f451c5a32b9aea33febaa58a
annavishnyakova <vishnyakova0307@gmail.com>
2023-03-20 19:32:26 +0300

    поправила дз

--- tests/ui_demoshop/test_for_demoshop.py
+++ tests/ui_demoshop/test_for_demoshop.py
@@ -3,7 +3,6 @@ import os
 import allure
 from dotenv import load_dotenv
 from selene import have
-from selene.support.shared import browser
 
 load_dotenv()
 
@@ -12,43 +11,43 @@ def test_login_with_api(browser_open):
     browser_open.open("")
 
     with allure.step("Verify successful authorization"):
-        browser.element(".account").should(have.text(os.getenv("LOGIN")))
+        browser_open.element(".account").should(have.text(os.getenv("LOGIN")))
 
 
 def test_search_negative_result(browser_open):
     browser_open.open("")
 
     with allure.step("Negitive search"):
-        browser.element('.search-box [value="Search store"]').click()
-        browser.element('.search-box [value="Search store"]').type('negative test').press_enter()
-        browser.element('.result').should(have.text('No products were found that matched your criteria.'))
+        browser_open.element('.search-box [value="Search store"]').click()
+        browser_open.element('.search-box [value="Search store"]').type('negative test').press_enter()
+        browser_open.element('.result').should(have.text('No products were found that matched your criteria.'))
 
 
 def test_watch_profile(browser_open):
     browser_open.open("")
 
     with allure.step("Check info in profile"):
-        browser.element(".account").should(have.text(os.getenv('LOGIN'))).click()
-        browser.element('#FirstName').should(have.value('mkdv'))
-        browser.element('#LastName').should(have.value('00'))
-        browser.element('#Email').should(have.value(os.getenv('LOGIN')))
-        browser.element('[checked="checked"]#gender-male')
+        browser_open.element(".account").should(have.text(os.getenv('LOGIN'))).click()
+        browser_open.element('#FirstName').should(have.value('mkdv'))
+        browser_open.element('#LastName').should(have.value('00'))
+        browser_open.element('#Email').should(have.value(os.getenv('LOGIN')))
+        browser_open.element('[checked="checked"]#gender-male')
 
 
 def test_watch_page_change_password(browser_open):
     browser_open.open("")
 
     with allure.step("Check text buttion in change password"):
-        browser.element(".account").should(have.text(os.getenv('LOGIN'))).click()
-        browser.element('[href="/customer/changepassword"]').should(have.text('Change password')).click()
-        browser.element('[for="OldPassword"]').should(have.text('Old password:'))
-        browser.element('[for="NewPassword"]').should(have.text('New password:'))
-        browser.element('[for="ConfirmNewPassword"]').should(have.text('Confirm password:'))
+        browser_open.element(".account").should(have.text(os.getenv('LOGIN'))).click()
+        browser_open.element('[href="/customer/changepassword"]').should(have.text('Change password')).click()
+        browser_open.element('[for="OldPassword"]').should(have.text('Old password:'))
+        browser_open.element('[for="NewPassword"]').should(have.text('New password:'))
+        browser_open.element('[for="ConfirmNewPassword"]').should(have.text('Confirm password:'))
 
 
 def test_logout(browser_open):
     browser_open.open("")
 
     with allure.step("Check logout"):
-        browser.element('.ico-logout').click()
-        browser.element('.ico-login').should(have.text('Log in'))
+        browser_open.element('.ico-logout').click()
+        browser_open.element('.ico-login').should(have.text('Log in'))
